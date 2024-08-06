7534db1e0b629d496eabd8932e62de8e163ac0c1
5RoD <5rod@gravemc.net>
2024-08-06 06:59:04 +0200

    added some comments to bossBar

--- src/main/java/rod/sentryx/events/bossBar.java
+++ src/main/java/rod/sentryx/events/bossBar.java
@@ -4,43 +4,47 @@ import org.bukkit.Bukkit;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 
-public class bossBar implements Listener{
+/**
+ * Manages the visibility of a BossBar based on player bed events.
+ */
+public class bossBar implements Listener {
 
-    public BossBar boss;
+    private final BossBar bossBar;
 
+    /**
+     * Creates a new instance of the BossBarManager.
+     */
     public bossBar() {
-        boss = Bukkit.createBossBar("You are sleeping now!", BarColor.GREEN, BarStyle.SOLID);
+        bossBar = Bukkit.createBossBar("You are sleeping now!", BarColor.GREEN, BarStyle.SOLID);
     }
 
-
+    /**
+     * Handles the PlayerBedEnterEvent by adding the player to the BossBar.
+     *
+     * @param event The PlayerBedEnterEvent.
+     */
     @EventHandler
-    private void test(PlayerBedEnterEvent e) {
-
-        if (e.getBedEnterResult().equals(PlayerBedEnterEvent.BedEnterResult.OK)) {
-
-            Player player = e.getPlayer();
-            boss.addPlayer(player);
-
-
+    private void handlePlayerBedEnter(PlayerBedEnterEvent event) {
+        if (event.getBedEnterResult().equals(PlayerBedEnterEvent.BedEnterResult.OK)) {
+            Player player = event.getPlayer();
+            bossBar.addPlayer(player);
         }
     }
 
+    /**
+     * Handles the PlayerBedLeaveEvent by removing the player from the BossBar.
+     *
+     * @param event The PlayerBedLeaveEvent.
+     */
     @EventHandler
-    private void leaveBed(PlayerBedLeaveEvent e) {
-        Player player = e.getPlayer();
-
-        if (boss != null) {
-            boss.removePlayer(player);
-
-
-        }
+    private void handlePlayerBedLeave(PlayerBedLeaveEvent event) {
+        Player player = event.getPlayer();
+        bossBar.removePlayer(player);
     }
-
 }
